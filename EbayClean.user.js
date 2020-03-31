41154787cde558a62060e965c7293ad5e23a7c18
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-03-31 16:14:51 +0200

    Update EbayClean.user.js

--- EbayClean.user.js
+++ EbayClean.user.js
@@ -7,17 +7,17 @@
 // @include     /^https?:\/\/(www\.)?ebay\.(?:com|co\.uk|\de)\/itm/.*/
 // @run-at      document-start
 // @icon        https://avatars3.githubusercontent.com/u/2756925?v=3&s=200
-// @homepageURL https://github.com/Duckle29/AliClean
+// @homepageURL https://github.com/Duckle29/EbayClean
 // @downloadURL https://raw.githubusercontent.com/Duckle29/EbayClean/master/EbayClean.user.js
 // @updateURL   https://raw.githubusercontent.com/Duckle29/EbayClean/master/EbayClean.user.js
-// @version     1.1
+// @version     1.2
+// @version     1.2 Fixed homepage URL
 // @history     1.1 Added support for .co.uk and .de
 // @history     1.0 Initial version
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    
+    'use strict';    
     var groups = document.URL.match(/^(https?:\/\/(?:www\.)?ebay\.(?:com|co\.uk|\de)\/itm)(?:\/[0-9a-zA-Z\-]+)(\/\d+)/);
     
     if (groups.length === 3 && groups[1]+groups[2] != document.URL)
