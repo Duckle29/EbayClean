25260fcde8dfbac8552bff0c940e3f6dfab8b22c
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-03-31 16:16:19 +0200

    Update EbayClean.user.js

--- EbayClean.user.js
+++ EbayClean.user.js
@@ -4,14 +4,15 @@
 // @license     BSD 3-Clause
 // @author      Duckle29
 // @namespace   https://github.com/Duckle29
-// @include     /^https?:\/\/(www\.)?ebay\.(?:com|co\.uk|\de)\/itm/.*/
+// @include     /^https?:\/\/(www\.)?ebay\.(?:com|co\.uk|de)\/itm/.*/
 // @run-at      document-start
 // @icon        https://avatars3.githubusercontent.com/u/2756925?v=3&s=200
 // @homepageURL https://github.com/Duckle29/EbayClean
 // @downloadURL https://raw.githubusercontent.com/Duckle29/EbayClean/master/EbayClean.user.js
 // @updateURL   https://raw.githubusercontent.com/Duckle29/EbayClean/master/EbayClean.user.js
-// @version     1.2
-// @version     1.2 Fixed homepage URL
+// @version     1.3
+// @history     1.3 Fixed .de regex
+// @history     1.2 Fixed homepage URL
 // @history     1.1 Added support for .co.uk and .de
 // @history     1.0 Initial version
 // ==/UserScript==
