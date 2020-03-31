d8c6656bfd4dcac6e83c2c051367c5cf18f8cec1
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-03-31 16:10:35 +0200

    Update EbayClean.user.js

--- EbayClean.user.js
+++ EbayClean.user.js
@@ -4,20 +4,21 @@
 // @license     BSD 3-Clause
 // @author      Duckle29
 // @namespace   https://github.com/Duckle29
-// @include     /^https?:\/\/(www\.)?ebay\.com\/itm/.*/
+// @include     /^https?:\/\/(www\.)?ebay\.(?:com|co\.uk|\de)\/itm/.*/
 // @run-at      document-start
 // @icon        https://avatars3.githubusercontent.com/u/2756925?v=3&s=200
 // @homepageURL https://github.com/Duckle29/AliClean
 // @downloadURL https://raw.githubusercontent.com/Duckle29/EbayClean/master/EbayClean.user.js
 // @updateURL   https://raw.githubusercontent.com/Duckle29/EbayClean/master/EbayClean.user.js
-// @version     1.0
+// @version     1.1
+// @history     1.1 Added support for .co.uk and .de
 // @history     1.0 Initial version
 // ==/UserScript==
 
 (function() {
     'use strict';
     
-    var groups = document.URL.match(/^(https?:\/\/(?:www\.)?ebay.com\/itm)(?:\/[0-9a-zA-Z\-]+)(\/\d+)/);
+    var groups = document.URL.match(/^(https?:\/\/(?:www\.)?ebay\.(?:com|co\.uk|\de)\/itm)(?:\/[0-9a-zA-Z\-]+)(\/\d+)/);
     
     if (groups.length === 3 && groups[1]+groups[2] != document.URL)
     {
